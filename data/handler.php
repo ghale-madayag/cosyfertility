c178552c60df5a86dd2ebd330b477c9d5943c2df
Ghale Madayag <ghale.madayag@gmail.com>
2019-07-20 19:35:18 +0800

    update connection

--- data/handler.php
+++ data/handler.php
@@ -2,7 +2,7 @@
 	// error_reporting(E_ALL ^ E_DEPRECATED);
 	// error_reporting(0);
 	try {
-		$handler = new PDO('mysql:host=localhost;dbname=cosy_db','root','');
+		$handler = new PDO('mysql:host=localhost;dbname=cosy_db','cosy_db_admin','L08hh0OP^.o{');
 		$handler->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $e) {
 		echo $e->getMessage();
